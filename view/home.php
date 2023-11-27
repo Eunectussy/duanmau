62e4ab1b778d8dc35f1389fdfd34361977251686
Eunectussy <147464538+Eunectussy@users.noreply.github.com>
2023-11-27 18:07:38 +0700

    Update home.php

--- view/home.php
+++ view/home.php
@@ -1,3 +1,12 @@
+<div class="banner" >
+    <div class="hinh"> 
+        <img src="https://theme.hstatic.net/1000306633/1000891824/14/block_home_category1_new.png?v=587" alt="" /></div>
+    <div class="hinh"> <img src="https://theme.hstatic.net/1000306633/1000891824/14/block_home_category2_new.png?v=587" alt="" /></div>
+    <div class="hinh"> <img src="https://theme.hstatic.net/1000306633/1000891824/14/block_home_category3_new.png?v=587" alt="" /></div>
+  </div>
+  <div class="typewriter">
+  <h1 id="text">STREETWEAR BRAND LIMITED</h1>
+  </div>
 <div class="nd">
              <?php
     $i=0;
@@ -11,17 +20,37 @@ if (isset($img_path)) {
 //     <p>'.$price.'</p>
 //     <a href="'.$linksp.'">'.$names.'</a>
 // </div>';
-echo'   <div class="card" style=" border:none;">
-<div class="imgBx ">
+echo'   <div class="card">
+<div class="imgBx">
 <a href="'.$linksp.'"><img src="'.$hinh.'" alt="nike-air-shoe" width="400px"/></a>
     
 </div>
 
 <div class="contentBx">
 </div>
-<div class="button"><a class="button-link primary-button" href="#">Buy Now</a>
-    <a class="button-link secondary-button" href="#">Thêm vào giỏ</a>
-</div>
+
+
+<form action="index.php?act=addtocart" method="post">
+    <input type="hidden" name="id" value="'.$id.'">
+    <input type="hidden" name="name" value="'.$names.'">
+    <input type="hidden" name="img" value="'.$img.'">
+    <input type="hidden" name="price" value="'.$price.'">
+    <div class="button">
+    <input class="button-link secondary-button" type="submit" name="addtocart" value="Thêm vào giỏ">
+    </div>
+</form>
+<form action="index.php?act=buynow" method="post">
+    <input type="hidden" name="id" value="'.$id.'">
+    <input type="hidden" name="name" value="'.$names.'">
+    <input type="hidden" name="img" value="'.$img.'">
+    <input type="hidden" name="price" value="'.$price.'">
+    <div class="button">
+    <input class="button-link secondary-button" type="submit" name="buynow" value="Mua ngay">
+    </div>
+</form>
+
+
+
 
 <div class="name"><h2>'.$price.'Vnđ</h2></div>
 <div class="size">
